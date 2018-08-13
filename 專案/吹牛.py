372aeab78dec515ac52682fe40d25a536d967464
yeamao <henry3556108@gmail.com>
2018-08-13 10:24:20 +0800

    可以開始玩了

--- 專案/吹牛.py
+++ 專案/吹牛.py
@@ -7,11 +7,12 @@ class Player():
         self.name=x
     def Name(self):
         return self.name
+    def Data(self):
+        return self.data
 
 class Game():
     def __init__(self):
         self.member=[]
-        self.temp=[]
         self.i=0
         self.time=0
     def Join(self,player):
@@ -30,10 +31,12 @@ class DiceGame(Game):
     def StartGame(self):
         stander=[]
         while True:
-            print(len(self.member),self.member[self.i].Name(),stander,self.i)
             inpu=input("input your chooce:").split(",")
             if self.ReviewInput(inpu)==True:
-                if inpu[0]==self.member[self.i].Name():
+                if inpu[1]=="抓":
+                    self.WinOrLose(self.i,inpu,stander)
+                    break
+                elif inpu[0]==self.member[self.i].Name():
                     if self.time==0:
                         stander=[int(inpu[1]),int(inpu[2])]
                         self.time=1
@@ -50,37 +53,51 @@ class DiceGame(Game):
                                 self.i=0
                             else:
                                 self.i=self.i+1
-                elif inpu[0]=="抓":
-                    self.WinOrLose(i)
                 else :
                     print("error input please try again")
-                if stander[0]>=6 and stander[1]>=6:
+                if stander[0]==6 and stander[1]==6:
                     inpu=input("input:").split(",")
-                    self.WinOrLose(i)
+                    self.WinOrLose(i,inpu,stander)
                     break
-            else: 
-                print(self.ReviewInput(inpu))
-    def WinOrLose(self,i):
-        print("in winorlose")
+            
+    def WinOrLose(self,i,inpu,stander):
+        yrdata=self.member[self.i-1].Data()
+        num1=yrdata.count(1)
+        numinpu=yrdata.count(stander[1])
+        print(yrdata,num1,numinpu)
+        if numinpu+num1<int(stander[0]):
+            print("player {} is the winner".format(inpu[0]))
+        else:
+            print("player {} you fail,player {} you win\nplayer {} dice is {}".format(inpu[0],self.member[self.i-1].Name(),self.member[self.i-1].Name(),yrdata))
     def ReviewInput(self,inpu):
-        if len(inpu)!=1 and len(inpu) != 3:
-            return "your input was error \nplease try again"
-        elif inpu=="抓":
+        if len(inpu)!=2 and len(inpu) != 3:
+            print("your input was error \nplease try again")
+        elif inpu[0] not in temp:
+            print("you are not player")
+        elif inpu[1]=="抓":
             if self.time == 0:
-                return"you have input value\nplease try again"
+                print("you have input value\nplease try again")
+            elif inpu[0] == self.member[self.i-1].Name():
+                print("you can't use 抓 this function")
+            else: return True    
         elif inpu[1].isdigit()==False or inpu[2].isdigit()==False:
             return "you have invade input\nplease try again"
-                
+        elif int(inpu[1])<1 or int(inpu[2])<1:
+            return "you can't play like this"
+        elif int(inpu[1])>6 or int(inpu[2])>6:
+            return "your input outoff range"
         else:
             return True
 Dice=DiceGame()
+temp=[]
 while(True): 
     inpu=input("input:").split("/")
     if inpu[0] == "start":
         break
     elif inpu[1] == "join":
+        temp.append(inpu[0])
         inpu[0]=Player(inpu[0])
-        Dice.member.append(inpu[0])
+        Dice.Join(inpu[0])
     else:
         print("error inpu")
 Dice.StartData()
